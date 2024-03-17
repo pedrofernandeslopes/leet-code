fa8ced4a106e3e8ff544aed581d900f6d24e44da
pedro <pedroflopes1710@gmail.com>
2024-03-17 19:42:29 -0300

    1929. Concatenation of Array

---
+++ java-solutions/src/arrays/ex01/Solution.java
@@ -0,0 +1,18 @@
+// 1929. Concatenation of Array
+
+package arrays.ex01;
+
+public class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int [n * 2];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+
+    
+}
